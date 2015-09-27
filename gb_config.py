1983b22b8dc58bb82e36963426999118c5f774e1
Jeremy Low <jeremylow@gmail.com>
2015-09-27 09:36:00 -0400

    fix os.envir to os.getenv

--- gb_config.py
+++ gb_config.py
@@ -8,18 +8,18 @@ MGS = {
     'instructions': 'Instructions are here: http://iseverythingstilltheworst.com/the-gifing-bot/'
 }
 
-CONSUMER_KEY = getattr(os.environ, "CONSUMER_KEY", "test")
-CONSUMER_SECRET = getattr(os.environ, "CONSUMER_SECRET", "test")
-ACCESS_KEY = getattr(os.environ, "ACCESS_KEY", "test")
-ACCESS_SECRET = getattr(os.environ, "ACCESS_SECRET", "test")
+CONSUMER_KEY = os.getenv("CONSUMER_KEY", "test")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET", "test")
+ACCESS_KEY = os.getenv("ACCESS_KEY", "test")
+ACCESS_SECRET = os.getenv("ACCESS_SECRET", "test")
 
-IMGUR_CLIENT_ID = getattr(os.environ, "IMGUR_CLIENT_ID", "test")
-IMGUR_CLIENT_SECRET = getattr(os.environ, "IMGUR_CLIENT_SECRET", "test")
+IMGUR_CLIENT_ID = os.getenv("IMGUR_CLIENT_ID", "test")
+IMGUR_CLIENT_SECRET = os.getenv("IMGUR_CLIENT_SECRET", "test")
 
-IMGUR_ACCESS_TOKEN = getattr(os.environ, "IMGUR_ACCESS_TOKEN", "test")
-IMGUR_REFRESH_TOKEN = getattr(os.environ, "IMGUR_REFRESH_TOKEN", "test")
+IMGUR_ACCESS_TOKEN = os.getenv("IMGUR_ACCESS_TOKEN", "test")
+IMGUR_REFRESH_TOKEN = os.getenv("IMGUR_REFRESH_TOKEN", "test")
 
 # Pretty basic for now, but maybe add more info in future.
 IMGUR_UPLOAD_CONFIG = {
-    'album': getattr(os.environ, "IMGUR_ALBUM", "test"),
+    'album': os.getenv("IMGUR_ALBUM", "test"),
 }
