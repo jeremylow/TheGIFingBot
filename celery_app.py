07996e87403237429caa4aa3d2bcb8a26fc7cc38
Jeremy Low <jeremylow@gmail.com>
2015-10-25 14:23:21 -0400

    updates to celery conf

--- celery_app.py
+++ celery_app.py
@@ -1,10 +1,15 @@
+from __future__ import absolute_import
 from celery import Celery
 
 # instantiate Celery object
-app = Celery(include=['gifing_bot_tasks'])
+app = Celery(
+    'TheGIFingBot',
+    broker='amqp://',
+    backend='amqp://',
+    include=['gifing_bot_tasks'])
 
 # import celery config file
 app.config_from_object('celery_config')
 
 if __name__ == '__main__':
-    pass
+    app.start()
